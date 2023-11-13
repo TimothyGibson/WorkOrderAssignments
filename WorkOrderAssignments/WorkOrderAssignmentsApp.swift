d6fd6cbcc31822161e74b84c15482fd247d88ead
TimothyGibson <tgibsonfl@gmail.com>
2023-11-12 19:14:41 -0500

    Initial Commit

---
+++ WorkOrderAssignments/WorkOrderAssignmentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkOrderAssignmentsApp.swift
+//  WorkOrderAssignments
+//
+//  Created by Tim Gibson on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct WorkOrderAssignmentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
